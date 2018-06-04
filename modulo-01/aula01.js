27000746260b392737182e446d9bf9949a42d015
Erandir Junior <aefs12junior@gmail.com>
2018-06-04 16:00:17 -0300

    Escopo no JS

---
+++ modulo-01/aula01.js
@@ -0,0 +1,14 @@
+// Escopo no Javascript
+
+var animal;
+
+function sayAnimal() {
+	animal = 'cat';
+	console.log(animal);
+}
+
+sayAnimal();
+console.log(animal);
+
+let sound = 'neow';
+const name = 'jake';
