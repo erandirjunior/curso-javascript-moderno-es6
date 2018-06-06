2325172c28494fec702d17931fb3aff3b39806d9
Erandir Junior <aefs12junior@gmail.com>
2018-06-06 19:16:52 -0300

    Introdução ao Map

---
+++ modulo-12/aula04.js
@@ -0,0 +1,9 @@
+// Introdução ao Map -> similar ao outros, mas são definidos chave e valor
+
+let m = new Map();
+
+m.set('Erandir, 22');
+m.set('Jonas, 35');
+m.set('Pedro, 17');
+
+console.log(m); //Map(3) {"Erandir, 22" => undefined, "Jonas, 35" => undefined, "Pedro, 17" => undefined}
