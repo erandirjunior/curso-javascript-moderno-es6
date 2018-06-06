6687c7f6f660be6afd0b576b494f5e9bb1bc5ab8
Erandir Junior <aefs12junior@gmail.com>
2018-06-06 12:55:01 -0300

    Destructuring em arrays

---
+++ modulo-06/aula02.js
@@ -0,0 +1,10 @@
+// Destructuring em arrays
+
+const arr = ['Erandir', 'Junior', 22, 'Fortaleza'];
+
+const [name, surname, age, city] = arr;
+
+console.log(name); //Erandir
+console.log(surname); //Junior
+console.log(age); //22
+console.log(city); //Fortaleza
