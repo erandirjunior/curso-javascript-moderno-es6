63cbe3b7586a886e8fed21c12200b63bbdbc597b
Erandir Junior <aefs12junior@gmail.com>
2018-06-06 21:06:09 -0300

    Trabalhando com exports no es6

--- modulo-13/app.js
+++ modulo-13/app.js
@@ -2,6 +2,7 @@
 // * carrega tudo da lib
 // as -> alias para o método (novo nome)
 import { union as juntaTudo, uniq as osMesmos } from 'ramda';
+import soma, { sub, multiplicacao, div as dividir, PI } from './utils';
 
 const arr1 = [1, 1, 1, 2, 2, 3, 4, 5, 6, 6];
 const arr2 = [5, 6, 6, 6, 7, 7, 8, 9, 10, -1];
@@ -13,3 +14,13 @@ const arr4 = osMesmos(arr1);
 console.log(arr3); //(11) [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -1]
 
 console.log(arr4); //(6) [1, 2, 3, 4, 5, 6]
+
+console.log(soma(5, 3)); //8
+
+console.log(sub(5, 3)); //2
+
+console.log(multiplicacao(5, 3)); //15
+
+console.log(dividir(6, 3)); //2
+
+console.log(PI); //3.14
